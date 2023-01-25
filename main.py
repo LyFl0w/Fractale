0570be4bffcd7f33853f0d09e95ec9284eb5122e
Fl0rely <82835327+Fl0rely@users.noreply.github.com>
2023-01-25 22:15:13 +0100

    Update main.py
    Amélioration zoom plus précis et utilisable mais pas encore parfait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import pygame
 import numpy as np
-
+import pygame
 
 def fractale_matrice(h, w, zoom=1.0, maxit=20, center_x=0, center_y=0, fractal_type='Mandelbrot', fractal_power=2):
     x, y = np.mgrid[-1.4:1.4:h * 1j, -2:0.8:w * 1j]
@@ -24,23 +23,21 @@ def fractale_matrice(h, w, zoom=1.0, maxit=20, center_x=0, center_y=0, fractal_t
         divtime[np.logical_and(diverge, divtime == maxit)] = i
         z[diverge] = 2
 
-    return divtime
+    return np.fliplr(np.flipud(divtime))
 
 
-
-native_h, native_w = 400, 400
-h, w = 400, 400
-centre=[200,200]
-fps = 100
+native_h, native_w = 600, 500
+h, w = 200,200
+centre=[0,0]
+fps = 50
 zoom = 1
 
 pygame.init()
 screen = pygame.display.set_mode((native_w, native_h))
 clock = pygame.time.Clock()
 
-
 def update(center_x, center_y):
-    mb = fractale_matrice(h, w, zoom, center_x=(center_x/native_w)*3.8-2, center_y=(center_y/native_h )*2.8-1.4, fractal_power=10, maxit=100)
+    mb = fractale_matrice(h, w, zoom, center_x=(center_x/native_w)*3.8, center_y=(center_y/native_h )*2.8, fractal_power=2, maxit=100)
     # Inversion de l'ordre des lignes de l'image de l'ensemble de Mandelbrot pour corriger la différence de coordonnées entre Pygame et NumPy
     mb = np.flipud(mb)
     mb_surface = pygame.surfarray.make_surface(mb)
@@ -50,35 +47,20 @@ def update(center_x, center_y):
     pygame.draw.circle(screen, (255, 255, 0), (native_w / 2, native_h / 2), 10)
     pygame.display.update()
 
-
-def zoom_at_cursor(zoom_factor):
-    global zoom,centre
-    #prend les coordonnées du curseur 
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    print("souris",mouse_x,mouse_y)
-    print("centre",centre)
-    #transforme les coordonnées en natif 
-    #mouse_x=(mouse_x/(native_w/w))
-    #mouse_y=(mouse_y/(native_h/h))
-    print("deplacement",(mouse_y-centre[1])*0.75,(mouse_x-centre[0])*0.75)
-    centre=[centre[0]+((mouse_x-centre[0])*0.2) , centre[1]+((mouse_y-centre[1])*0.2)]
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    # Calculate the offset of the cursor from the current center
-    offset_x = (mouse_x - centre[0]) / zoom
-    offset_y = (mouse_y - centre[1]) / zoom
-    # Update the center to keep the cursor in the same position
-    centre[0] += offset_x * (zoom_factor - 1)
-    centre[1] += offset_y * (zoom_factor - 1)
-    zoom*=zoom_factor
-    print(centre)
-    update(centre[0],centre[1  ])
-    #
-
+def zoom_at_cursor(factor):
+    global zoom, centre
+    cursor_x, cursor_y = pygame.mouse.get_pos()
+    cursor_x_fractale = (cursor_x/native_w)*3.8
+    cursor_y_fractale = (cursor_y/native_h)*2.8
+    centre[0] += (cursor_x_fractale - centre[0]) * (1 - factor)
+    centre[1] += (cursor_y_fractale - centre[1]) * (1 - factor)
+    zoom *= factor
+    update(centre[0], centre[1])
 
 def move(dx, dy):
     global centre
     speed = 1
-    centre = [centre[0] - dx * speed, centre[1] + dy * speed]
+    centre = [centre[0] + dx * speed, centre[1] - dy * speed]
     print("mouvement souris",centre[0],centre[1])
     update(centre[0], centre[1])
 
@@ -89,7 +71,7 @@ def handle_mouse_movement():
         move(dx, dy)
 
 
-update((2/3.8)*native_h, 1.4/5*native_w)
+update(centre[0],centre[1])
 
 running = True
 while running:
