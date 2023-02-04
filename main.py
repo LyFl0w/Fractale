406c08b8114870ac1030265df0706856afe8a91b
Fl0rely <82835327+Fl0rely@users.noreply.github.com>
2023-02-04 15:33:20 +0100

    Update main.py
    changement système de coordonnées en passant de (0,0) en haut à gauche à (0,0) au millieu et amélioration du zoom

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ filtre = pygame.Color(0, 255, 0)
 native_h, native_w = 500, 500
 h, w = 400, 400
 centre = [0, 0]
+diff=[h/2,w/2]
 fps = 50
 zoom = 1
 
@@ -12,23 +13,25 @@ pygame.init()
 screen = pygame.display.set_mode((native_w, native_h))
 clock = pygame.time.Clock()
 
-def fractale_matrice(h, w, zoom=1.0, maxit=20, top_left_x=0, top_left_y=0,screen=screen):
+
+def fractale_matrice(h, w, zoom=1.0, maxit=20, x=0, y=0,screen=screen):
     if maxit>0:
-        pygame.draw.rect(screen,(255,255,255),(top_left_x+(w/3),top_left_y+(h/3),(h/3)*zoom,(w/3)*zoom),0)
-        for i in range(3):
-            for j in range(3):
-                if (i,j)!=(1,1):
-                    fractale_matrice(h/3,w/3,zoom,maxit-1,top_left_x+(w/3)*i,top_left_y+(h/3)*j,screen)
+        pygame.draw.rect(screen,(255,255,255),( ((x-(h/3/2))*zoom+diff[0]),((y-(w/3/2))*zoom+diff[1]),int((w/3)*zoom ),int((h/3)*zoom)),0)
+        for i in [-1,0,1]:
+            for j in [-1,0,1]:
+                if (i,j)!=(0,0):
+                    fractale_matrice(h/3,w/3,zoom,maxit-1,x+(w/3)*i,y+(h/3)*j,screen)
 
 def zoom_at_cursor(zoom_factor):
     global zoom, centre
     zoom*=zoom_factor
+    print(zoom)
     update(centre[0],centre[1])
 
 def update(center_x, center_y):
     #screen.fill(0,0,0)
     mb = pygame.Surface((w,h))
-    fractale_matrice(h,w,zoom=zoom,maxit=5,top_left_x=center_x,top_left_y=center_y,screen=mb)
+    fractale_matrice(h,w,zoom=zoom,maxit=5,x=center_x,y=center_y,screen=mb)
     # Inversion de l'ordre des lignes de l'image de l'ensemble de Mandelbrot pour corriger la différence de coordonnées entre Pygame et NumPy
     screen.blit(pygame.transform.scale(mb, (native_w, native_h)), (0, 0))
     pygame.display.update()
