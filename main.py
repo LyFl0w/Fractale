323272710b091c3ac7aff3c2de3b682f260bb527
LyFlow <off.lyflow@gmail.com>
2023-02-07 19:18:27 +0100

    + ajout du systeme de filtre de couleur

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import numpy as np
 import pygame
 
-filtre = pygame.Color(0, 255, 0)
+filtre = pygame.Color(255, 0, 0)
 native_h, native_w = 500, 500
 h, w = 400, 400
 centre = [0, 0]
-diff=[h/2,w/2]
+diff = [h / 2, w / 2]
 fps = 50
 zoom = 1
 
@@ -14,40 +13,48 @@ screen = pygame.display.set_mode((native_w, native_h))
 clock = pygame.time.Clock()
 
 
-def fractale_matrice(h, w, zoom=1.0, maxit=20, x=0, y=0,screen=screen):
-    if maxit>0:
-        pygame.draw.rect(screen,(255,255,255),( ((x-(h/3/2))*zoom+diff[0]),((y-(w/3/2))*zoom+diff[1]),int((w/3)*zoom ),int((h/3)*zoom)),0)
-        for i in [-1,0,1]:
-            for j in [-1,0,1]:
-                if (i,j)!=(0,0):
-                    fractale_matrice(h/3,w/3,zoom,maxit-1,x+(w/3)*i,y+(h/3)*j,screen)
+def fractale_matrice(h, w, zoom=1.0, maxit=20, x=0, y=0, screen=screen):
+    if maxit > 0:
+        pygame.draw.rect(screen, (255, 255, 255), (
+        ((x - (h / 3 / 2)) * zoom + diff[0]), ((y - (w / 3 / 2)) * zoom + diff[1]), int((w / 3) * zoom),
+        int((h / 3) * zoom)), 0)
+        for i in [-1, 0, 1]:
+            for j in [-1, 0, 1]:
+                if (i, j) != (0, 0):
+                    fractale_matrice(h / 3, w / 3, zoom, maxit - 1, x + (w / 3) * i, y + (h / 3) * j, screen)
+
 
 def zoom_at_cursor(zoom_factor):
     global zoom, centre
-    zoom*=zoom_factor
+    zoom *= zoom_factor
     print(zoom)
-    update(centre[0],centre[1])
+    update(centre[0], centre[1])
+
 
 def update(center_x, center_y):
-    #screen.fill(0,0,0)
-    mb = pygame.Surface((w,h))
-    fractale_matrice(h,w,zoom=zoom,maxit=5,x=center_x,y=center_y,screen=mb)
+    # screen.fill(0,0,0)
+    mb = pygame.Surface((w, h))
+    fractale_matrice(h, w, zoom=zoom, maxit=5, x=center_x, y=center_y, screen=mb)
     # Inversion de l'ordre des lignes de l'image de l'ensemble de Mandelbrot pour corriger la différence de coordonnées entre Pygame et NumPy
     screen.blit(pygame.transform.scale(mb, (native_w, native_h)), (0, 0))
+    screen.fill(filtre, special_flags=8)
     pygame.display.update()
+
 
 def handle_mouse_movement():
     dx, dy = pygame.mouse.get_rel()
     if pygame.mouse.get_pressed()[0]:
         move(dx, dy)
 
+
 def move(dx, dy):
     global centre
-    speed = 1/zoom
+    speed = 1 / zoom
     centre = [centre[0] + dx * speed, centre[1] + dy * speed]
     update(centre[0], centre[1])
 
-update(centre[0],centre[1])
+
+update(centre[0], centre[1])
 pygame.display.update()
 running = True
 while running:
