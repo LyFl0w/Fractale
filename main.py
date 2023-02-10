6c5f66f7cf9a9f4f259e95a2082714b088aaceda
Fl0rely <82835327+Fl0rely@users.noreply.github.com>
2023-02-10 19:14:34 +0100

    Update main.py
    début du zoom infini mais bug

--- main.py
+++ main.py
@@ -1,60 +1,116 @@
+import numpy as np
 import pygame
 
-filtre = pygame.Color(255, 0, 0)
+filtre = pygame.Color(0, 255, 0)
 native_h, native_w = 500, 500
 h, w = 400, 400
-centre = [0, 0]
-diff = [h / 2, w / 2]
+dim_screen=[400,400]
+diff=[h/2,w/2]
 fps = 50
-zoom = 1
+
+
+centre = [-134, 0]
+zoom = 9
+maxit=6
 
 pygame.init()
 screen = pygame.display.set_mode((native_w, native_h))
 clock = pygame.time.Clock()
 
+def verifier_presence(centre_screen,h_screen,w_screen,centre_carre,h_carre,w_carre):
+    for i in [0]:
+        for j in [0]:
+            #print()
+            #if centre_carre[0]<0 and centre_carre[1]<0:
+                #print(centre_screen)
+                #print(centre_screen[0]-(w_screen/2),centre_carre[0]+(w_carre/2)*i,centre_screen[0]+(w_screen/2)," ", centre_screen[1]-(h_screen/2),centre_carre[1]+(h_carre/2)*j,centre_screen[1]+(h_screen/2))
+            if centre_screen[0]-(w_screen/2)<=centre_carre[0]+(w_carre/2)*i<=centre_screen[0]+(w_screen/2) or centre_screen[1]-(h_screen/2)<=centre_carre[1]+(h_carre/2)*j<=centre_screen[1]+(h_screen/2):
+                return True 
+    return False
 
-def fractale_matrice(h, w, zoom=1.0, maxit=20, x=0, y=0, screen=screen):
-    if maxit > 0:
-        pygame.draw.rect(screen, (255, 255, 255), (
-        ((x - (h / 3 / 2)) * zoom + diff[0]), ((y - (w / 3 / 2)) * zoom + diff[1]), int((w / 3) * zoom),
-        int((h / 3) * zoom)), 0)
-        for i in [-1, 0, 1]:
-            for j in [-1, 0, 1]:
-                if (i, j) != (0, 0):
-                    fractale_matrice(h / 3, w / 3, zoom, maxit - 1, x + (w / 3) * i, y + (h / 3) * j, screen)
-
+def fractale_matrice(h_carre, w_carre, zoom=1.0, maxit=maxit, x=0, y=0,screen=screen):
+    a=0
+    if maxit>0:
+        a+=1
+        pygame.draw.rect(screen,(255,255,255),( ((x-(h_carre/3/2))*zoom+diff[0]),((y-(w_carre/3/2))*zoom+diff[1]),int((w_carre/3)*zoom ),int((h_carre/3)*zoom)),0)
+        for i in [-1,0,1]:
+            for j in [-1,0,1]:
+                if (i,j)!=(0,0):
+                    #if verifier_presence((centre[0],centre[1]),dim_screen[1],dim_screen[0],(x+(w_carre/3)*i,y+(h_carre/3)*j),h_carre/3,w_carre/3)==True:
+                    a+=fractale_matrice(h_carre/3,w_carre/3,zoom,maxit-1,x+(w_carre/3)*i,y+(h_carre/3)*j,screen)
+    return a
 
 def zoom_at_cursor(zoom_factor):
-    global zoom, centre
-    zoom *= zoom_factor
-    print(zoom)
-    update(centre[0], centre[1])
+    global zoom, centre,co_screen,dim_screen
+    zoom*=zoom_factor
+    dim_screen=[w/zoom,h/zoom]
+    #print(zoom)
+    #print(dim_screen)
+    update(centre[0],centre[1])
 
+
+def position_cube(center_x,center_y,cube_x,cube_y,largeur):
+    if center_x-cube_x>=largeur/2:
+        x=1
+    elif center_x-cube_x<=-largeur/2:
+        x=-1
+    else:
+        x=0
+    if center_y-cube_y>=largeur:
+        y=1
+    elif center_y-cube_y<=-largeur:
+        y=-1
+    else:
+        y=0
+    return [x,y]
+
+
+def infini(center_x,center_y):
+    global zoom,centre,maxit,pos_cube
+    mb = pygame.Surface((w,h))
+    pos_cube=position_cube(center_x,center_y,0,0,h/3)
+    bloc=position_cube(center_x,center_y,pos_cube[0]*h/3,pos_cube[1]*h/3,h/9)
+    if zoom<=1:
+        zoom*=3
+        maxit=5
+        center_x=h/3*bloc[0]+((pos_cube[0]*h/3+bloc[0]*h/9)-center_x)*3
+        center_y=h/3*bloc[1]+((pos_cube[1]*h/3+bloc[1]*h/9)-center_y)*3
+    elif zoom>=9:
+        zoom/=3
+        maxit=5
+        center_x=h/3*bloc[0]+((pos_cube[0]*h/3+bloc[0]*h/9)-center_x)*3
+        center_y=h/3*bloc[1]+((pos_cube[1]*h/3+bloc[1]*h/9)-center_y)*3
+
+    print("case",pos_cube)
+    print("bloc",bloc)
+    return [center_x,center_y]
 
 def update(center_x, center_y):
-    # screen.fill(0,0,0)
-    mb = pygame.Surface((w, h))
-    fractale_matrice(h, w, zoom=zoom, maxit=5, x=center_x, y=center_y, screen=mb)
+    #screen.fill(0,0,0)
+    mb = pygame.Surface((w,h))
+    print("-------------------------")
+    centre=infini(center_x,center_y)
+    fractale_matrice(h,w,zoom=zoom,maxit=maxit,x=-center_x,y=center_y,screen=mb)
     # Inversion de l'ordre des lignes de l'image de l'ensemble de Mandelbrot pour corriger la différence de coordonnées entre Pygame et NumPy
     screen.blit(pygame.transform.scale(mb, (native_w, native_h)), (0, 0))
-    screen.fill(filtre, special_flags=8)
-    pygame.display.update()
 
+    print("co",center_x,center_y)
+    print("zoom",zoom)
+    
+    pygame.display.update()
 
 def handle_mouse_movement():
     dx, dy = pygame.mouse.get_rel()
     if pygame.mouse.get_pressed()[0]:
         move(dx, dy)
 
-
 def move(dx, dy):
     global centre
-    speed = 1 / zoom
-    centre = [centre[0] + dx * speed, centre[1] + dy * speed]
+    speed = 1/zoom
+    centre = [centre[0] - dx * speed, centre[1] + dy * speed]
     update(centre[0], centre[1])
 
-
-update(centre[0], centre[1])
+update(centre[0],centre[1])
 pygame.display.update()
 running = True
 while running:
